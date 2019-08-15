a2ad0a8be61799a2c32e38328fccb8ddfed02ca7
Edwin Sanchez Pedro <j.edwinsp@gmail.com>
2019-08-15 17:54:27 -0500

    Update script.js
    Create function for convert HTML to PDF

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  html2canvas(document.getElementById("resume")).then(function(canvas) {
+    var imgData = canvas.toDataURL("image/png");
+    var imgWidth = 220;
+    var imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    var doc = new jsPDF();
+
+    doc.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+    doc.save("Resume.pdf");
+  });
+});
