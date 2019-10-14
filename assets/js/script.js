07430275fefffe7beb6f14cc2fec157fd5589250
Edwin Sanchez Pedro <j.edwinsp@gmail.com>
2019-10-14 14:08:45 -0500

    Prueba Page Company

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,73 @@
 $(document).ready(function() {
-  html2canvas(document.getElementById("resume")).then(function(canvas) {
-    var doc = new jsPDF("p", "mm", "a4");
-    var width = doc.internal.pageSize.getWidth();
-    var height = doc.internal.pageSize.getHeight();
-    var imgData = canvas.toDataURL("image/png");
-    doc.addImage(imgData, "PNG", 0, 0, width, height);
-    doc.save("Resume.pdf");
+  var w = window.innerWidth;
+
+  if (w > 767) {
+    $("#menu-jk").scrollToFixed();
+  } else {
+    $("#menu-jk").scrollToFixed();
+  }
+});
+
+$(document).ready(function() {
+  $("#home-slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 5000,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+  $("#testimonial-slider").owlCarousel({
+    items: 2,
+    itemsDesktop: [1000, 2],
+    itemsDesktopSmall: [992, 1],
+    itemsTablet: [768, 1],
+    pagination: false,
+    navigation: true,
+    navigationText: ["", ""],
+    autoPlay: true
   });
 });
+
+$(document).ready(function() {
+  $(".filter-button").click(function() {
+    var value = $(this).attr("data-filter");
+
+    if (value == "all") {
+      $(".filter").show("1000");
+    } else {
+      $(".filter")
+        .not("." + value)
+        .hide("3000");
+      $(".filter")
+        .filter("." + value)
+        .show("3000");
+    }
+  });
+
+  if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
+});
+
+function goToModal(title) {
+  $('#title-modal').text(title);
+  $('#content-modal').text('PLATAFORMA DE SOPORTE A LA GESTIÓN AGRÍCOLA Es una solución integral y modular para todos los procesos de la gestión agrícola adaptable a cualquier cultivo tanto para pequeños fundos como grandes extensiones de tierra brindándote información en linea y procesada para el análisis de todo lo que ocurre en el campo y posterior toma de decisiones.');
+}
