c6efb5a426395b2d81052645a64ba56685610856
Edwin Sanchez Pedro <j.edwinsp@gmail.com>
2019-10-21 15:22:32 -0500

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,69 @@ $(document).ready(function() {
     $("#menu-jk").scrollToFixed();
   }
 });
+
+$(document).ready(function() {
+	$(".jm-loadingpage").fadeOut("slow");
+});
+
+jQuery(document).ready(function() {
+	jQuery(".jm-loadingpage").fadeOut("slow");
+});
+
+function validateEmail(email) {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+
+function check(e) {
+	tecla = (document.all) ? e.keyCode : e.which;
+
+    if (tecla == 8 || tecla == 32) {
+        return true;
+	}
+	
+    patron = /[A-Za-z]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
+
+function valida(f) {
+	var ok = true;
+	var msg = "";
+
+	if (f.elements[0].value == "") {
+	  msg = "Por favor, ingrese su nombre.";
+	  ok = false;
+	  f.elements[0].focus();
+	} else if (!validateEmail(f.elements[1].value)) {
+		msg = "Por favor, ingrese un email correcto.";
+		ok = false;
+		f.elements[1].focus();
+	} else if (f.elements[2].value == "") {
+		msg = "Por favor, ingrese su número de celular.";
+		ok = false;
+		f.elements[2].focus();
+	} else if (f.elements[3].value == "") {
+		msg = "Por favor, ingrese su mensaje.";
+		ok = false;
+		f.elements[3].focus();
+	}
+  
+	if(ok == false) 
+		alertError(msg);
+	  
+	return ok;
+  }
+
+  function alertError(msg) {
+    Swal.fire({
+      position: "center",
+      type: "error",
+      title: "Error",
+      text: msg,
+      showConfirmButton: false,
+      timer: 2000
+	});
+	
+	$('#swal2-title').css("display", "inline");
+  }
